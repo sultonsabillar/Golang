36e0f1960bb86bc84c4a4d47d9a9b66f80cb98dc
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2024-02-13 19:31:47 +0700

    MiniCha 2

--- Mini challenge/MiniCha2.go
+++ Mini challenge/MiniCha2.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	// Variable yang berisi string kalimat
+	kalimat := "Halo teman-teman, apa kabar hari ini?"
+
+	// Pecah kalimat menjadi kata-kata
+	kataKata := strings.Fields(kalimat)
+
+	// Inisialisasi map untuk menyimpan jumlah kemunculan kata
+	kemunculanKata := make(map[string]int)
+
+	// Looping untuk menghitung kemunculan kata
+	for _, kata := range kataKata {
+		kemunculanKata[kata]++
+	}
+
+	// Tampilkan hasil perhitungan kemunculan kata
+	for kata, jumlah := range kemunculanKata {
+		fmt.Printf("Kata: %s, Jumlah: %d\n", kata, jumlah)
+	}
 }
