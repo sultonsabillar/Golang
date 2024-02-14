cf451a09114d3df3d9dd5af1968c4962c450b635
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2024-02-15 01:48:26 +0700

    exported-unexported init sesi 3

---
+++ Sesi 3/exported-unexported/init.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// create on a new project if you want to try or execute this "exported-unexported" lesson
+func init() {
+	fmt.Println("Hallo, ini berasal dari function init")
+}
