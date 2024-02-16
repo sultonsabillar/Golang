d49f2d31d7d75f3bdb975e02563c0d5500eab2ba
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2024-02-16 19:41:30 +0700

    ouput sudah sesuai

--- Mini challenge/MiniCha2.go
+++ Mini challenge/MiniCha2.go
@@ -21,35 +21,6 @@ func main() {
 		kemunculanHuruf[string(huruf)]++
 	}
 
-	// Tampilkan hasil perhitungan kemunculan huruf
-	fmt.Println("Hasil perhitungan kemunculan huruf:")
-	for huruf, jumlah := range kemunculanHuruf {
-		fmt.Printf("%s: %d\n", huruf, jumlah)
-	}
-}
-package main
-
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
-func main() {
-	// Membaca input dari pengguna
-	fmt.Print("Masukkan kalimat: ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	kalimat := scanner.Text()
-
-	// Inisialisasi map untuk menyimpan jumlah kemunculan huruf
-	kemunculanHuruf := make(map[string]int)
-
-	// Looping untuk menghitung kemunculan huruf
-	for _, huruf := range kalimat {
-		kemunculanHuruf[string(huruf)]++
-	}
-
 	// Tampilkan huruf satu per satu ke bawah
 	for _, huruf := range kalimat {
 		fmt.Println(string(huruf))
