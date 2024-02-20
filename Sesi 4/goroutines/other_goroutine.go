5c195bb82e83bc727f6226608788fecfbda85d80
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2024-02-20 21:27:35 +0700

    other_goroutines sesi 4

---
+++ Sesi 4/goroutines/other_goroutine.go
@@ -0,0 +1,32 @@
+package main
+
+// func main() {
+// 	fmt.Println("Main execution started")
+
+// 	go firstProcess(5)
+
+// 	secondProcess(5)
+
+// 	fmt.Println("No. of Goroutines:", runtime.NumGoroutine())
+
+// 	time.Sleep(time.Second * 2)
+
+// 	fmt.Println("Main execution ended")
+// }
+
+// func firstProcess(index int) {
+// 	fmt.Println("First process func started")
+// 	for i := 1; i <= index; i++ {
+// 		fmt.Println("i =", i)
+// 	}
+// 	fmt.Println("First process func ended")
+
+// }
+
+// func secondProcess(index int) {
+// 	fmt.Println("Second process func started")
+// 	for j := 1; j <= index; j++ {
+// 		fmt.Println("j =", j)
+// 	}
+// 	fmt.Println("Second process func ended")
+// }
