49416a791e3dbfbd67b2845378d1f60b54a9b3fa
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2024-02-22 23:43:43 +0700

    Mini Challenge 4 combine

--- Mini challenge/MiniCha4.go
+++ Mini challenge/MiniCha4.go
@@ -3,6 +3,46 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
+)
+
+type Interface1 struct {
+	Data string
+}
+
+type Interface2 struct {
+	Data int
+}
+
+func processData1(data Interface1) {
+	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	fmt.Println("Processed Interface1 data:", data.Data)
+}
+
+func processData2(data Interface2) {
+	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+	fmt.Println("Processed Interface2 data:", data.Data)
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < 4; i++ {
+		data1 := Interface1{"Data1"}
+		data2 := Interface2{42}
+
+		go processData1(data1)
+		go processData2(data2)
+	}
+
+	time.Sleep(1 * time.Second) // Waiting for goroutines to finish
+}
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
 	"sync"
 	"time"
 )
