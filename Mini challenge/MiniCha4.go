c4af89813828a74761f86a56dc967bc346d696c0
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2024-02-22 23:44:26 +0700

    Coba test

--- Mini challenge/MiniCha4.go
+++ Mini challenge/MiniCha4.go
@@ -38,54 +38,54 @@ func main() {
 	time.Sleep(1 * time.Second) // Waiting for goroutines to finish
 }
 
-package main
+// package main
 
-import (
-	"fmt"
-	"math/rand"
-	"sync"
-	"time"
-)
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"sync"
+// 	"time"
+// )
 
-type Interface1 struct {
-	Data string
-}
+// type Interface1 struct {
+// 	Data string
+// }
 
-type Interface2 struct {
-	Data int
-}
+// type Interface2 struct {
+// 	Data int
+// }
 
-var mutex sync.Mutex
+// var mutex sync.Mutex
 
-func processData1(data Interface1, wg *sync.WaitGroup) {
-	defer wg.Done()
-	mutex.Lock()
-	defer mutex.Unlock()
-	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
-	fmt.Println("Processed Interface1 data:", data.Data)
-}
+// func processData1(data Interface1, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	mutex.Lock()
+// 	defer mutex.Unlock()
+// 	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+// 	fmt.Println("Processed Interface1 data:", data.Data)
+// }
 
-func processData2(data Interface2, wg *sync.WaitGroup) {
-	defer wg.Done()
-	mutex.Lock()
-	defer mutex.Unlock()
-	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
-	fmt.Println("Processed Interface2 data:", data.Data)
-}
+// func processData2(data Interface2, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	mutex.Lock()
+// 	defer mutex.Unlock()
+// 	time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+// 	fmt.Println("Processed Interface2 data:", data.Data)
+// }
 
-func main() {
-	rand.Seed(time.Now().UnixNano())
+// func main() {
+// 	rand.Seed(time.Now().UnixNano())
 
-	var wg sync.WaitGroup
-	wg.Add(8)
+// 	var wg sync.WaitGroup
+// 	wg.Add(8)
 
-	for i := 0; i < 4; i++ {
-		data1 := Interface1{"Data1"}
-		data2 := Interface2{42}
+// 	for i := 0; i < 4; i++ {
+// 		data1 := Interface1{"Data1"}
+// 		data2 := Interface2{42}
 
-		go processData1(data1, &wg)
-		go processData2(data2, &wg)
-	}
+// 		go processData1(data1, &wg)
+// 		go processData2(data2, &wg)
+// 	}
 
-	wg.Wait() // Waiting for all goroutines to finish
-}
+// 	wg.Wait() // Waiting for all goroutines to finish
+// }
