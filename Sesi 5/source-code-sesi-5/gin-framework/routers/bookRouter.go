55d7b22e091d27d218d6b4a4d94ebcbf980242b1
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2024-02-25 20:58:06 +0700

    bookrouter sesi 5

---
+++ Sesi 5/source-code-sesi-5/gin-framework/routers/bookRouter.go
@@ -0,0 +1,21 @@
+package routers
+
+import (
+	"gin-framework/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func StartServer() *gin.Engine {
+	router := gin.Default()
+
+	router.POST("/book", controllers.CreateBook)
+
+	router.GET("/book/:bookID", controllers.GetBook)
+
+	router.PUT("/book/:bookID", controllers.UpdateBook)
+
+	router.DELETE("/book/:bookID", controllers.DeleteBook)
+
+	return router
+}
