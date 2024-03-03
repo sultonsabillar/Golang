7a0e133bac7a2488b7f499806274a7c6749bb161
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2024-03-03 23:44:20 +0700

    Mini Cha 6 coba

--- Mini challenge/MiniCha6.go
+++ Mini challenge/MiniCha6.go
@@ -21,7 +21,38 @@ func main() {
 			fmt.Println("Square")
 		case isCube:
 			fmt.Println("Cube")
-		default:
+		default:package main
+
+		import (
+			"bufio"
+			"fmt"
+			"os"
+		)
+		
+		func main() {
+			// Membaca input dari pengguna
+			fmt.Print("Masukkan kalimat: ")
+			scanner := bufio.NewScanner(os.Stdin)
+			scanner.Scan()
+			kalimat := scanner.Text()
+		
+			// Inisialisasi map untuk menyimpan jumlah kemunculan huruf
+			kemunculanHuruf := make(map[string]int)
+		
+			// Looping untuk menghitung kemunculan huruf
+			for _, huruf := range kalimat {
+				kemunculanHuruf[string(huruf)]++
+			}
+		
+			// Tampilkan huruf satu per satu ke bawah
+			for _, huruf := range kalimat {
+				fmt.Println(string(huruf))
+			}
+		
+			// Tampilkan hasil perhitungan kemunculan huruf
+			fmt.Println("\n", kemunculanHuruf)
+		}
+		
 			fmt.Println(i)
 		}
 	}
