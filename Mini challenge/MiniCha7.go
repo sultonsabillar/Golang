b787fd228a42d7eb1fc1aeb5c6065687706fd366
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2024-03-04 23:32:19 +0700

    Mini Cha 7 test

--- Mini challenge/MiniCha7.go
+++ Mini challenge/MiniCha7.go
@@ -28,7 +28,38 @@ func main() {
 }
 
 // Fungsi untuk mengecek apakah suatu angka adalah kuadrat sempurna
-func isPerfectSquare(num int) bool {
+func isPerfectSquare(num int) boolpackage main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Membaca input dari pengguna
+	fmt.Print("Masukkan kalimat: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	kalimat := scanner.Text()
+
+	// Inisialisasi map untuk menyimpan jumlah kemunculan huruf
+	kemunculanHuruf := make(map[string]int)
+
+	// Looping untuk menghitung kemunculan huruf
+	for _, huruf := range kalimat {
+		kemunculanHuruf[string(huruf)]++
+	}
+
+	// Tampilkan huruf satu per satu ke bawah
+	for _, huruf := range kalimat {
+		fmt.Println(string(huruf))
+	}
+
+	// Tampilkan hasil perhitungan kemunculan huruf
+	fmt.Println("\n", kemunculanHuruf)
+}
+ {
 	root := int(math.Sqrt(float64(num)))
 	return root*root == num
 }
