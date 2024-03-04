f771405e2bc5388862b01b9089eab7991112fb00
Sulton Sabilla Rosad <sultonsabillar@gmail.com>
2024-03-04 23:32:50 +0700

    Mini Cha 7 coba

--- Mini challenge/MiniCha7.go
+++ Mini challenge/MiniCha7.go
@@ -2,70 +2,53 @@ package main
 
 import (
 	"fmt"
-	"math"
-)
-
-func main() {
-	var n int
-	fmt.Print("Masukkan nilai n: ")
-	fmt.Scanln(&n)
-
-	for i := 1; i <= n; i++ {
-		isSquare := isPerfectSquare(i)
-		isCube := isPerfectCube(i)
-
-		switch {
-		case isSquare && isCube:
-			fmt.Println("SquareCube")
-		case isSquare:
-			fmt.Println("Square")
-		case isCube:
-			fmt.Println("Cube")
-		default:
-			fmt.Println(i)
-		}
-	}
-}
-
-// Fungsi untuk mengecek apakah suatu angka adalah kuadrat sempurna
-func isPerfectSquare(num int) boolpackage main
-
-import (
-	"bufio"
-	"fmt"
 	"os"
 )
 
+// Struct untuk menyimpan data teman
+type Teman struct {
+	Nama      string
+	Alamat    string
+	Pekerjaan string
+	Alasan    string
+}
+
+// Data teman-teman di kelas
+var temanTeman = []Teman{
+	{"Fitri", "Jakarta", "Developer", "Ingin mempelajari Golang untuk pengembangan aplikasi backend."},
+	{"Budi", "Bandung", "Designer", "Tertarik dengan kemampuan konkurensi dan kinerja yang cepat pada Golang."},
+
+// Function untuk mencari teman berdasarkan nama
+func cariTeman(nama string) (Teman, bool) {
+	for _, teman := range temanTeman {
+		if teman.Nama == nama {
+			return teman, true
+		}
+	}
+	return Teman{}, false
+}
+
 func main() {
-	// Membaca input dari pengguna
-	fmt.Print("Masukkan kalimat: ")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	kalimat := scanner.Text()
-
-	// Inisialisasi map untuk menyimpan jumlah kemunculan huruf
-	kemunculanHuruf := make(map[string]int)
-
-	// Looping untuk menghitung kemunculan huruf
-	for _, huruf := range kalimat {
-		kemunculanHuruf[string(huruf)]++
+	// Mendapatkan argumen dari CLI
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Println("Harap berikan nama teman sebagai argumen CLI.")
+		return
 	}
 
-	// Tampilkan huruf satu per satu ke bawah
-	for _, huruf := range kalimat {
-		fmt.Println(string(huruf))
+	// Mengambil nama teman dari argumen CLI
+	namaTeman := args[1]
+
+	// Mencari teman berdasarkan nama
+	teman, found := cariTeman(namaTeman)
+	if !found {
+		fmt.Printf("Data dengan nama %s tidak tersedia.\n", namaTeman)
+		return
 	}
 
-	// Tampilkan hasil perhitungan kemunculan huruf
-	fmt.Println("\n", kemunculanHuruf)
-}
- {
-	root := int(math.Sqrt(float64(num)))
-	return root*root == num
-}
-
-// Fungsi untuk mengecek apakah suatu angka adalah kubus sempurna
-func isPerfectCube(num int) bool {
-	root := int(math.Round(math.Pow(float64(num), 1.0/3.0)))
-	return root*root*root == num
+	// Menampilkan data teman
+	fmt.Println("Nama:", teman.Nama)
+	fmt.Println("Alamat:", teman.Alamat)
+	fmt.Println("Pekerjaan:", teman.Pekerjaan)
+	fmt.Println("Alasan memilih kelas Golang:", teman.Alasan)
 }
